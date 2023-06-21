932a7e4bba88093ba0a2317728218ec485cd05f7
Paul <colandreapaul@gmail.com>
2023-06-21 14:56:12 -0400

    add performance testing

--- App.tsx
+++ App.tsx
@@ -1,6 +1,12 @@
 import { StatusBar } from "expo-status-bar";
 import { useState } from "react";
-import { Text, TouchableOpacity, View, ViewStyle } from "react-native";
+import {
+  LayoutAnimation,
+  Text,
+  TouchableOpacity,
+  View,
+  ViewStyle,
+} from "react-native";
 import BIP32Factory from "bip32";
 import { wordlist } from "@scure/bip39/wordlists/english";
 
@@ -24,12 +30,34 @@ export default function App() {
   });
 
   const onClickmePressed = () => {
+    let start = performance.now();
     const entropy = crypto.getRandomValues(new Uint8Array(16));
-    const mnemonic = bip39.entropyToMnemonic(entropy, wordlist);
+    let end = performance.now();
+    console.log(`[Entropy Generation] ${end - start}ms`);
 
+    start = performance.now();
+    const mnemonic = bip39.entropyToMnemonic(entropy, wordlist);
+    end = performance.now();
+    console.log(`[Mnemonic Generation] ${end - start}ms`);
+
+    // SLOW
+    start = performance.now();
     const seed = bip39.mnemonicToSeedSync(mnemonic);
+    end = performance.now();
+    console.log(`[Seed Sync] ${end - start}ms`);
+    // ==
+
+    start = performance.now();
     const root = bip32.fromSeed(Buffer.from(seed), bitcoin.networks.bitcoin);
+    end = performance.now();
+    console.log(`[Seed Derivation] ${end - start}ms`);
+
+    start = performance.now();
     const child1 = root.derivePath(BTC_DERIVATION_PATH);
+    end = performance.now();
+    console.log(`[Path Derivation] ${end - start}ms\n\n=====\n\n`);
+
+    LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut);
 
     setWallet({
       address: bitcoin.payments.p2wpkh({
@@ -44,12 +72,14 @@ export default function App() {
       <StatusBar style="auto" />
       <Text style={{ fontSize: 30 }}>CRYPTO testing</Text>
 
-      <TouchableOpacity onPress={onClickmePressed} style={$button}>
-        <Text style={{ color: "white" }}>Click me</Text>
-      </TouchableOpacity>
-
       <Text>{wallet.address}</Text>
       <Text>{wallet.key}</Text>
+
+      <TouchableOpacity onPress={onClickmePressed} style={$button}>
+        <Text style={{ color: "white", fontSize: 17, fontWeight: "600" }}>
+          Generate Wallet
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -64,8 +94,21 @@ const $root: ViewStyle = {
 const $button: ViewStyle = {
   backgroundColor: "#1E1E1E",
   borderRadius: 20,
-  width: "50%",
-  height: 30,
+  width: "90%",
+  height: 50,
+  alignSelf: "center",
+  position: "absolute",
+  bottom: 50,
   justifyContent: "center",
   alignItems: "center",
+
+  shadowColor: "red",
+  shadowOffset: {
+    width: 0,
+    height: 2,
+  },
+  shadowOpacity: 0.5,
+  shadowRadius: 12.35,
+
+  elevation: 19,
 };
