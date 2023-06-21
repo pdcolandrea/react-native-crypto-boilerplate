3c0951c078e404eaf4b2ed55a32e65a6c5dc23b9
Paul <colandreapaul@gmail.com>
2023-06-21 17:02:16 -0400

    working adr generation

--- App.tsx
+++ App.tsx
@@ -1,13 +1,16 @@
 import { StatusBar } from "expo-status-bar";
 import { useState } from "react";
 import {
+  FlatList,
+  Image,
   LayoutAnimation,
   Text,
+  TextStyle,
   TouchableOpacity,
   View,
   ViewStyle,
 } from "react-native";
-import BIP32Factory from "bip32";
+import BIP32Factory, { BIP32Interface } from "bip32";
 import { wordlist } from "@scure/bip39/wordlists/english";
 
 import * as ecc from "@bitcoinerlab/secp256k1";
@@ -17,19 +20,26 @@ import * as bitcoin from "bitcoinjs-lib";
 const bip32 = BIP32Factory(ecc);
 
 interface WalletState {
-  address: string;
+  addresses: string[];
+  root: BIP32Interface | null;
   key: string;
+  adrIndex: number;
 }
 
-const BTC_DERIVATION_PATH = "m/84'/0'/0'/0/0";
+const BTC_DERIVATION_PATH = "m/84'/0'/0'/0";
 
+// TODO: FLATLIST SWIPE NOT WORKING
 export default function App() {
   const [wallet, setWallet] = useState<WalletState>({
-    address: "",
+    addresses: [],
+    root: null,
     key: "",
+    adrIndex: 0,
   });
 
   const onClickmePressed = () => {
+    LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut);
+
     let start = performance.now();
     const entropy = crypto.getRandomValues(new Uint8Array(16));
     let end = performance.now();
@@ -53,27 +63,175 @@ export default function App() {
     console.log(`[Seed Derivation] ${end - start}ms`);
 
     start = performance.now();
-    const child1 = root.derivePath(BTC_DERIVATION_PATH);
+    const child1 = root.derivePath(`${BTC_DERIVATION_PATH}/${wallet.adrIndex}`);
     end = performance.now();
     console.log(`[Path Derivation] ${end - start}ms\n\n=====\n\n`);
 
-    LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut);
+    setWallet((prev) => {
+      return {
+        addresses: [
+          bitcoin.payments.p2wpkh({
+            pubkey: child1.publicKey,
+          }).address,
+        ],
+        key: mnemonic,
+        adrIndex: prev.adrIndex + 1,
+        root,
+      };
+    });
+  };
 
-    setWallet({
-      address: bitcoin.payments.p2wpkh({
-        pubkey: child1.publicKey,
-      }).address,
-      key: mnemonic,
+  const onGenerateAddressPressed = () => {
+    if (wallet.adrIndex >= 7) return;
+
+    const index = wallet.adrIndex + 1;
+    const newChild = wallet.root.derivePath(`${BTC_DERIVATION_PATH}/${index}`);
+
+    const newAddress = bitcoin.payments.p2wpkh({
+      pubkey: newChild.publicKey,
+    }).address;
+
+    setWallet((prev) => {
+      return {
+        ...prev,
+        adrIndex: index,
+        addresses: [...prev.addresses, newAddress],
+      };
     });
   };
 
   return (
     <View style={$root}>
-      <StatusBar style="auto" />
-      <Text style={{ fontSize: 30 }}>CRYPTO testing</Text>
+      <StatusBar style="dark" />
+      <View
+        style={{
+          alignSelf: "flex-start",
+        }}
+      >
+        <Text style={{ fontSize: 40, color: "white", fontWeight: "800" }}>
+          C R Y P T O
+        </Text>
+        <Text
+          style={{
+            color: "white",
+            fontSize: 15,
+            marginTop: -5,
+            alignSelf: "flex-end",
+          }}
+        >
+          bytefederal testing
+        </Text>
+      </View>
 
-      <Text>{wallet.address}</Text>
-      <Text>{wallet.key}</Text>
+      <FlatList
+        data={[1, 2, 3]}
+        contentContainerStyle={{
+          flex: 1,
+          alignContent: "stretch",
+          marginTop: 24,
+          marginBottom: 120,
+        }}
+        snapToAlignment="start"
+        horizontal
+        pagingEnabled
+        snapToInterval={100}
+        renderItem={({}) => (
+          <View
+            style={{
+              backgroundColor: "#131313",
+              width: "100%",
+              borderRadius: 24,
+              padding: 12,
+            }}
+          >
+            <View
+              style={{
+                width: "100%",
+                flexDirection: "row",
+                flexWrap: "wrap",
+                justifyContent: "center",
+              }}
+            >
+              {wallet.key.split(" ").map((word, index) => {
+                if (word === "") return;
+
+                return (
+                  <View
+                    style={{
+                      height: 30,
+                      paddingHorizontal: 12,
+                      backgroundColor: "black",
+                      justifyContent: "center",
+                      borderRadius: 12,
+                      margin: 2,
+                    }}
+                    key={index}
+                  >
+                    <Text style={{ color: "white", fontSize: 12 }}>{word}</Text>
+                  </View>
+                );
+              })}
+            </View>
+
+            <View style={{ marginTop: 12 }}>
+              <Text style={{ fontSize: 17, color: "white", fontWeight: "700" }}>
+                Addresses
+              </Text>
+
+              {wallet.addresses.map((adr, index) => {
+                return (
+                  <View key={adr} style={[$row, { marginVertical: 6 }]}>
+                    <View
+                      style={{
+                        borderRadius: 320,
+                        borderColor: "red",
+                        borderWidth: 1,
+                        marginRight: 5,
+                        height: 20,
+                        width: 20,
+                        justifyContent: "center",
+                        alignItems: "center",
+                      }}
+                    >
+                      <Text style={{ color: "white" }}>{index}</Text>
+                    </View>
+
+                    <Text style={[$bodyText]}>{adr}</Text>
+                  </View>
+                );
+              })}
+
+              {wallet.addresses.length !== 0 && (
+                <TouchableOpacity
+                  onPress={onGenerateAddressPressed}
+                  style={{
+                    borderColor: "rgba(255, 255, 255, 0.5)",
+                    borderWidth: 1,
+                    alignSelf: "center",
+                    paddingLeft: 12,
+                    paddingRight: 12,
+                    paddingVertical: 5,
+                    borderRadius: 12,
+                    flexDirection: "row",
+                  }}
+                >
+                  <Text style={[$bodyText, { fontSize: 12 }]}>
+                    Generate New Address
+                  </Text>
+                  <Image
+                    source={require("./assets/plus.png")}
+                    style={{
+                      width: 15,
+                      height: 15,
+                      marginLeft: 5,
+                    }}
+                  />
+                </TouchableOpacity>
+              )}
+            </View>
+          </View>
+        )}
+      />
 
       <TouchableOpacity onPress={onClickmePressed} style={$button}>
         <Text style={{ color: "white", fontSize: 17, fontWeight: "600" }}>
@@ -86,9 +244,9 @@ export default function App() {
 
 const $root: ViewStyle = {
   flex: 1,
-  backgroundColor: "#fff",
+  backgroundColor: "black",
   padding: 12,
-  marginTop: 60,
+  paddingTop: 60,
 };
 
 const $button: ViewStyle = {
@@ -111,4 +269,13 @@ const $button: ViewStyle = {
   shadowRadius: 12.35,
 
   elevation: 19,
+};
+
+const $bodyText: TextStyle = {
+  color: "white",
+  fontSize: 13,
+};
+
+const $row: ViewStyle = {
+  flexDirection: "row",
 };
